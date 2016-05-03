0a8548f66e7b127b33e171e42650ed5664153d03
Robert Marianski <rob@marianski.com>
2016-05-03 17:30:38 -0400

    Add more commands

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 P=toi
 OBJECTS=$(P).o
-CFLAGS = `pkg-config --cflags hiredis futile` -g -Wall -std=c11 -pedantic -O3
+CFLAGS = `pkg-config --cflags hiredis futile` -g -Wall -std=gnu11 -O3
 LDLIBS = `pkg-config --libs hiredis futile`
 
 $(P): $(OBJECTS)
@@ -8,11 +8,17 @@ $(P): $(OBJECTS)
 %.o: %.c
 	$(CC) -c $(CFLAGS) $(CPPFLAGS) $< -o $@
 
-run: $(P)
-	./$(P) localhost
+print: $(P)
+	./$(P) -b toi.bin -c print
+
+hash: $(P)
+	./$(P) -b toi.bin -c hash
+
+dump: $(P)
+	./$(P) -r localhost -c dump -d toi.bin
 
 clean:
 	rm -f $(OBJECTS) $(P)
 
 valgrind: $(P)
-	G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind --leak-check=full ./$(P) localhost
+	G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind --leak-check=full ./$(P) -b toi.bin -c print
--- toi.c
+++ toi.c
@@ -1,5 +1,11 @@
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <hiredis/hiredis.h>
@@ -10,7 +16,7 @@ typedef struct {
     size_t n;
 } toi_s;
 
-toi_s fetch_coord_ints(char *redis_host) {
+toi_s read_toi_redis(char *redis_host) {
     redisContext *context = redisConnect(redis_host, 6379);
     if (context != NULL && context->err) {
         printf("Error: %s\n", context->errstr);
@@ -41,6 +47,36 @@ toi_s fetch_coord_ints(char *redis_host) {
     return toi;
 }
 
+toi_s read_toi_bin(char *filename) {
+    struct stat stat_buf;
+    if (stat(filename, &stat_buf) != 0) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+    FILE *fin = fopen(filename, "r");
+    if (!fin) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    off_t bin_size = stat_buf.st_size;
+    unsigned int n_coord_ints = bin_size / sizeof(uint64_t);
+    uint64_t *coord_ints = malloc(bin_size);
+    for (size_t i = 0; i < n_coord_ints; i++) {
+        int n_read = fread(coord_ints + i, sizeof(uint64_t), 1, fin);
+        if (n_read != 1) {
+            perror("fread");
+            exit(EXIT_FAILURE);
+        }
+    }
+    int rc = fclose(fin);
+    if (rc) {
+        perror("fclose");
+        exit(EXIT_FAILURE);
+    }
+    toi_s result = {.coord_ints = coord_ints, .n=n_coord_ints};
+    return result;
+}
+
 void free_toi(toi_s toi) {
     free(toi.coord_ints);
 }
@@ -64,20 +100,184 @@ void print_zoom_counts(toi_s toi) {
     }
 }
 
-int main(int argc, char *argv[]) {
+typedef struct toi_hash_entry_s {
+    uint64_t coord_int;
+    struct toi_hash_entry_s *next;
+} toi_hash_entry_s;
 
-    if (argc != 2) {
-        printf("Usage: %s <redis-host>\n", argv[0]);
+unsigned int calc_hash(uint64_t coord_int) {
+    // TODO experiment here
+    // return coord_int;
+    // return 0;
+    futile_coord_s coord;
+    futile_coord_unmarshall_int(coord_int, &coord);
+    return coord.z * 7 + coord.x * 5 + coord.y * 3;
+}
+
+void print_hash_stats(toi_hash_entry_s **table, size_t hash_size) {
+    const size_t lengths_size = 10000;
+    unsigned int lengths[lengths_size];
+    memset(lengths, 0, sizeof(lengths));
+    for (size_t entry_index = 0; entry_index < hash_size; entry_index++) {
+        toi_hash_entry_s *entry = table[entry_index];
+        if (entry) {
+            unsigned int length = 0;
+            while (entry) {
+                length++;
+                entry = entry->next;
+            }
+            if (length > lengths_size) {
+                length = lengths_size;
+            }
+            lengths[length-1]++;
+        }
+    }
+    for (size_t length_index = 0; length_index < lengths_size; length_index++) {
+        unsigned int length = lengths[length_index];
+        if (length > 0) {
+            printf("%zu: %u\n", length_index + 1, length);
+        }
+    }
+}
+
+void hash(toi_s toi) {
+    toi_hash_entry_s *entries = malloc(sizeof(toi_hash_entry_s) * toi.n);
+
+    size_t hash_size = pow(2, 24);
+    toi_hash_entry_s **table = malloc(sizeof(*(toi_hash_entry_s *)0) * hash_size);
+    memset(table, 0, sizeof(*(toi_hash_entry_s *)0) * hash_size);
+
+    for (size_t toi_index = 0; toi_index < toi.n; toi_index++) {
+        toi_hash_entry_s *entry = entries + toi_index;
+        entry->coord_int = toi.coord_ints[toi_index];
+
+        unsigned int hashcode = calc_hash(entry->coord_int);
+        size_t bucket = hashcode & (hash_size - 1);
+
+        toi_hash_entry_s *location = table[bucket];
+        if (location) {
+            entry->next = location;
+        } else {
+            entry->next = NULL;
+        }
+        table[bucket] = entry;
+    }
+
+    print_hash_stats(table, hash_size);
+    free(table);
+    free(entries);
+}
+
+void dump(toi_s toi, char *filename) {
+    FILE *fout = fopen(filename, "w");
+    if (!fout) {
+        perror("fopen");
         exit(EXIT_FAILURE);
     }
 
-    char *redis_host = argv[1];
+    for (size_t toi_index = 0; toi_index < toi.n; toi_index++) {
+        int n_written = fwrite(toi.coord_ints + toi_index, sizeof(uint64_t), 1, fout);
+        if (n_written != 1) {
+            perror("fwrite");
+            exit(EXIT_FAILURE);
+        }
+        assert(n_written == 1);
+    }
 
-    toi_s toi = fetch_coord_ints(redis_host);
+    int rc = fclose(fout);
+    if (rc) {
+        perror("fclose");
+        exit(EXIT_FAILURE);
+    }
+}
 
-    print_zoom_counts(toi);
+void usage(char *progname) {
+    fprintf(stderr, "Usage: %s [-r redis-host] [-b binary-file] [-c <print|hash|dump>] [-d dumpfile]\n",
+            progname);
+    exit(EXIT_FAILURE);
+}
+
+typedef struct {
+    int type; // 1 for redis, 2 for bin file
+    char *value; // redis host or filename
+} input_source_s;
+
+int main(int argc, char *argv[]) {
+
+    input_source_s input_source = {};
+    int command = 0; // print->1, hash->2, dump->3
+    int opt;
+    char *dump_filename = NULL;
+
+    while ((opt = getopt(argc, argv, "r:b:c:d:")) != -1) {
+        switch (opt) {
+        case 'r':
+            input_source.type = 1;
+            input_source.value = strdup(optarg);
+            break;
+        case 'b':
+            input_source.type = 2;
+            input_source.value = strdup(optarg);
+            break;
+        case 'c':
+            if (strcmp(optarg, "print") == 0) {
+                command = 1;
+            } else if (strcmp(optarg, "hash") == 0) {
+                command = 2;
+            } else if (strcmp(optarg, "dump") == 0) {
+                command = 3;
+            } else {
+                usage(argv[0]);
+            }
+            break;
+        case 'd':
+            dump_filename = strdup(optarg);
+            break;
+        default:
+            usage(argv[0]);
+        }
+    }
+
+    if (!input_source.type) {
+        fprintf(stderr, "Must specify either -r (redis host) or -b (binary file)\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!command) {
+        fprintf(stderr, "Must specify -c command <print|hash|dump>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    toi_s toi;
+    if (input_source.type == 1) {
+        toi = read_toi_redis(input_source.value);
+    } else if (input_source.type == 2) {
+        toi = read_toi_bin(input_source.value);
+    } else {
+        assert(!"Add input source type");
+    }
+
+    switch (command) {
+    case 1:
+        print_zoom_counts(toi);
+        break;
+    case 2:
+        hash(toi);
+        break;
+    case 3:
+        if (!dump_filename) {
+            fprintf(stderr, "Must specify -d with dump command\n");
+            exit(EXIT_FAILURE);
+        }
+        dump(toi, dump_filename);
+        break;
+    default:
+        assert(!"Add command");
+    }
 
     free_toi(toi);
+    free(dump_filename);
+    free(input_source.value);
 
     return 0;
 }
